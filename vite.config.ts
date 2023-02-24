ce1c7c0d681a1decb113177aad3b1a4ef997e4fb
ajay-paramlogic <80455750+ajay-paramlogic@users.noreply.github.com>
2023-02-24 13:30:12 +0530

    Icons and SplashScreen ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,49 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PwApp',
+        short_name: 'pwapp',
+        description: 'United PwApp',
+        theme_color: '#ffffff',
+        background_color: '#ade8f4',
+        display: 'standalone',
+        start_url: '/',
+        lang: 'en',
+        scope: '/',
+        icons: [
+          {
+            src: 'pwa-assets/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-assets/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'pwa-assets/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-assets/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
